4936f0a4aaec9413a7ef2057cb930801c8865485
ouassimkads <ouassimkadissa@outlook.com>
2025-05-12 02:41:33 +0100

    d

--- plugins/init-auth.client.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import { useAuthStore } from '~/stores/auth'
-
-export default defineNuxtPlugin(async () => {
-  if (import.meta.server) return;
-
-  const auth = useAuthStore()
-  await auth.fetchUser()
-})
