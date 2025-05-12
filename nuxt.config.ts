603ca273bd70adb389779dee327bb8b6874a669c
ouassimkads <ouassimkadissa@outlook.com>
2025-05-12 01:17:31 +0100

    add all like tailwind supabase daisyui nuxtui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
+    '@nuxt/ui',
     '@nuxt/icon',
-    '@nuxt/ui'
-  ]
+    '@nuxt/image',
+    '@pinia/nuxt',
+  ], 
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import colors from 'tailwindcss/colors'
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.green
+      }, 
+      fontFamily: {
+        roboto: ['Roboto', 'Mona Sans', 'sans-serif'],
+        opensans: ['"Open Sans"', 'sans-serif'],
+      },
+    }
+  }
+}
